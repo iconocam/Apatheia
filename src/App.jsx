b7090f79dbfeb0c9db22c5a5610e6ab4c0ce5876
iconocam <150183122+iconocam@users.noreply.github.com>
2024-02-06 17:55:03 -0500

    apatheia

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Home from "./components/Home"
+import Logo from "./components/Logo"
+import Navbar from "./components/Navbar"
+
+function App() {
+
+
+  return (
+    <> 
+    <Navbar />
+    <Home />
+    <Logo />
+    </>
+  )
+}
+
+export default App
